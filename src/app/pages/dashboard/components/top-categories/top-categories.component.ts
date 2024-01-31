754442e24a47442e7fa1d3f52280c57bc0248ba2
FatemehAhmadikia <fahmadikia96@gmail.com>
2024-01-31 16:41:55 +0330

    top categories

--- src/app/pages/dashboard/components/top-categories/top-categories.component.ts
+++ src/app/pages/dashboard/components/top-categories/top-categories.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Months } from '../../enums/dashboard.enum';
 
 @Component({
     selector: 'top-categories',
     templateUrl: './top-categories.component.html',
     styleUrl: './top-categories.component.scss',
 })
-export class TopCategoriesComponent {}
+export class TopCategoriesComponent {
+    months: string[] = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+    selectedMonth: Months = Months.May;
+
+    get Months() {
+        return Months;
+    }
+}
---
+++ src/app/pages/dashboard/enums/dashboard.enum.ts
@@ -0,0 +1,13 @@
+export enum Months {
+    January = 'January',
+    February = 'February',
+    March = 'March',
+    April = 'April',
+    May = 'May',
+    June = 'June',
+    July = 'July',
+    September = 'September',
+    October = 'October',
+    November = 'November',
+    December = 'December',
+}
